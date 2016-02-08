81a9b30a5080b32dbd2b145d433ed470a8db40f3
remcohh <remcohh@gmail.com>
2016-02-08 11:18:37 +0100

    Add start-stop

--- js/app.js
+++ js/app.js
@@ -2,4 +2,14 @@ $( document ).ready(function() {
     console.log("hoi")
     $('#second_line').css('margin-left', "-"+$("#score_window").width());
 
+    $("#start").click(function(){
+        left=0
+        stop=false
+        window.scoreScroll()
+    })
+
+    $("#stop").click(function(){
+        stop = true
+    })
+
 });
--- js/scroll.js
+++ js/scroll.js
@@ -1,6 +1,8 @@
 function scoreScroll() {
     left = left + 1
     $("#score_window").scrollLeft(left)
-    scrolldelay = setTimeout('scoreScroll()',15); // scrolls every 100 milliseconds
+    if(!stop) {
+        scrolldelay = setTimeout('scoreScroll()', 15); // scrolls every 100 milliseconds
+    }
 
 }
